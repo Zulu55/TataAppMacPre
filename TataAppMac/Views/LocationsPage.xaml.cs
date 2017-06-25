33baa004ae03562bbee2acc8e507ab2159fb7b49
Zulu55 <jzuluaga55@gmail.com>
2017-06-25 17:47:27 -0500

    Add new pin to map

--- TataAppMac/Views/LocationsPage.xaml.cs
+++ TataAppMac/Views/LocationsPage.xaml.cs
@@ -1,6 +1,8 @@
 ﻿namespace TataAppMac.Views
 {
+    using System;
     using System.Threading.Tasks;
+    using TataAppMac.Models;
     using TataAppMac.Serviices;
     using TataAppMac.ViewModels;
     using Xamarin.Forms;
@@ -8,16 +10,21 @@
 
     public partial class LocationsPage : ContentPage
     {
-        #region Attributes
-        GeolocatorService geolocatorService;
-        #endregion
+		#region Attributes
+		GeolocatorService geolocatorService;
+		ApiService apiService;
+		DialogService dialogService;
+		#endregion
 
-        #region Constructors
-        public LocationsPage()
+		#region Constructors
+		public LocationsPage()
         {
             InitializeComponent();
 
             geolocatorService = new GeolocatorService();
+            apiService = new ApiService();
+            dialogService = new DialogService();
+
             MoveToCurrentLocation();
         }
         #endregion
@@ -32,10 +39,10 @@
                 MyMap.MoveToRegion(MapSpan.FromCenterAndRadius(position, Distance.FromMiles(.3)));
             }
 
-			await ShowPins();
+			await ShowPoins();
 		}
 
-        async Task ShowPins()
+        async Task ShowPoins()
         {
             var locationsViewModel = LocationsViewModel.GetInstance();
             await locationsViewModel.LoadPins();
@@ -44,6 +51,82 @@
                 MyMap.Pins.Add(pin);
             }
         }
-        #endregion
-    }
+
+		void NewPinClicked(object sender, EventArgs args)
+		{
+			PinDescription.Text = string.Empty;
+			NewPinModal.IsVisible = true;
+			PinDescription.Focus();
+		}
+
+		async void SavePinClicked(object sender, EventArgs args)
+        {
+			if (string.IsNullOrEmpty(PinDescription.Text))
+			{
+				await dialogService.ShowMessage("Error", "You must enter a Pin description.");
+				return;
+			}
+
+			if (string.IsNullOrEmpty(PinAddress.Text))
+			{
+				await dialogService.ShowMessage("Error", "You must enter a Pin address.");
+				return;
+			}
+
+			await geolocatorService.GetLocation();
+			if (geolocatorService.Latitude == 0 && geolocatorService.Longitude == 0)
+			{
+				await dialogService.ShowMessage("Error", "The localization in not available.");
+				return;
+			}
+
+            var location = new Location
+            {
+                Address = PinAddress.Text,
+                Description = PinDescription.Text,
+                Latitude = geolocatorService.Latitude,
+                Longitude = geolocatorService.Longitude,
+            };
+
+			var urlAPI = Application.Current.Resources["URLAPI"].ToString();
+			var mainViewModel = MainViewModel.GetInstance();
+			var employee = mainViewModel.Employee;
+
+			var response = await apiService.Post(
+				urlAPI,
+				"/api",
+				"/Locations",
+				employee.TokenType,
+				employee.AccessToken,
+                location);
+
+			if (!response.IsSuccess)
+			{
+				await dialogService.ShowMessage("Error", "Problem ocurred saving the new location.");
+				return;
+			}
+
+            var position = new Position(geolocatorService.Latitude, geolocatorService.Longitude);
+
+            var pin = new Pin
+            {
+                Address = PinAddress.Text,
+                Label = PinDescription.Text,
+                Position = position,
+                Type = PinType.Place,
+            };
+
+			var locationsViewModel = LocationsViewModel.GetInstance();
+            locationsViewModel.Pins.Add(pin);
+		    MyMap.Pins.Add(pin);
+
+			NewPinModal.IsVisible = false;
+		}
+
+		void CancelPinClicked(object sender, EventArgs args)
+		{
+			NewPinModal.IsVisible = false;
+		}
+		#endregion
+	}
 }
